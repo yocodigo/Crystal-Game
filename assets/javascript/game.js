64f0fe55c3510cc4a2ce4fa57436995119dd8179
unknown <jjhernandez94@yahoo.com>
2018-01-23 20:24:31 -0800

    Crystal Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,62 @@
 $(document).ready(function(){
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+$("button").on("click", function() {
+        var myAudio = $("#myAudio");
+        if (myAudio.paused) {
+            myAudio.play();
+        } else {
+            myAudio.pause();
+        }
+ });	
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
 	//DECLARE & INITIALIZE GLOBAL VARIABLES
 	var computerPick = 0;
 	var totalScore = 0;
 	var wins = 0;
 	var losses = 0;
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
 	//COMPUTER'S RANDOM NUMBER GENERATOR
 	resetComputerPick();
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
-	//BUTTON RANDOM NUMBER GENERATOR 
-	resetButtonValues();
+	//CRYSTAL RANDOM NUMBER GENERATOR 
+	resetCrystalValues();
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	//ADD TO SCORE WHEN BUTTONS ARE PRESSED
-	$("button").on("click", function() {
+	//ADD TO SCORE WHEN CRYSTALS ARE CLICKED
+	$("img").on("click", function() {
 		lockGame = false;
+		$("#result").html("---------------");
 		if (lockGame !== true) {
 			yourPick = $(this).attr("value");
 			console.log("Your Pick: " + computerPick);
 			console.log("My Num: " + $(this).val());
 			totalScore = totalScore + parseInt(yourPick);
 			$("#score").html(totalScore);
-			console.log(totalScore == computerPick);
-		}	
-	
+			console.log("Greater than computer's pick? " + (totalScore == computerPick));
+		}		
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
 		//COMPARE YOUR PICK WITH COMPUTER'S PICK
 		if (totalScore == computerPick) {
-			$("#result").html("Yep! You got it! Refresh the page to play again.");
+			$("#result").html("You Win!");
 			resetComputerPick();
-			resetButtonValues();
+			resetCrystalValues();
 			wins++;
+			$("#win").html(wins);
+			totalScore = 0;
+			$("#score").html(totalScore);
+
 		}
 		else if (totalScore >= computerPick) {
-			$("#result").html("Nope. Refresh the page to play again.");
+			$("#result").html("You Lose!");
 			lockGame = true;
 			resetComputerPick();
-			resetButtonValues();
+			resetCrystalValues();
 			losses++;
-		}
+			$("#loss").html(losses);
+			console.log("you have " + losses + " losses!");
+			totalScore = 0;
+			$("#score").html(totalScore);
+	;	}
 	});//AddToScore Function closing brackets
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
 	//RESET COMPUTER'S PICK
@@ -53,17 +71,16 @@ $(document).ready(function(){
 		$("#computer-pick").text(computerPick);
 	}
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
-	//RESET BUTTON VALUES
-	function resetButtonValues() {
+	//RESET CRYSTAL VALUES
+	function resetCrystalValues() {
 	var numOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 		randomNum = numOptions[Math.floor(Math.random() * numOptions.length)]
 		randomNum2 = numOptions[Math.floor(Math.random() * numOptions.length)]
 		randomNum3 = numOptions[Math.floor(Math.random() * numOptions.length)]
 		randomNum4 = numOptions[Math.floor(Math.random() * numOptions.length)]
-		$("#button1").attr("value", randomNum);
-		$("#button2").attr("value", randomNum2);
-		$("#button3").attr("value", randomNum3);
-		$("#button4").attr("value", randomNum4);
+		$("#img1").attr("value", randomNum);
+		$("#img2").attr("value", randomNum2);
+		$("#img3").attr("value", randomNum3);
+		$("#img4").attr("value", randomNum4);
 	}
-	
 });//document ready closing brackets
